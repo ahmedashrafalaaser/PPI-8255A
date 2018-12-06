fa7274e0aeb886baabdeed4f1388464efcb73447
ahmedashrafalaaser <ahmedashrafalaaser@gmail.com>
2018-12-06 17:11:48 +0200

    ta3del 3  portcu

--- ppi/PortCu.v
+++ ppi/PortCu.v
@@ -24,7 +24,7 @@ module portcu(PCu,PD,control,controlword);
 	wire [3:0]PCuin;
 	reg [3:0]PCuout;
 	 
-	assign PCu=(controlword==8'b0xxx_xxxx||control==6'b010010&&controlword==8'b100x0xxx)?PCuout  :4'hz;
+	assign PCu=(controlword[7]==0||control==6'b010010&&controlword==8'b100x0xxx)?PCuout  :4'hz;
 	assign  PCuin=PCu;
 	assign PD=(control==6'b001010&&controlword==8'b100x1xxx)?{(4'hz),PCuin}:8'hzz;
 	
@@ -34,11 +34,11 @@ if(controlword[7]==1)
 		begin
 PCuout<={PD[7:4]};
 end
-if (controlword==8'b0xxx_xxxx&&control==6'bxxx0xx)
+if (controlword[7]==0&&control[2]==0)
 begin 
 	selectedport={controlword[3:1]};
 if(selectedport>4)
- PCuout[selectedport-4] =controlword[0]; //set or no the selected pin
+ PCuout[selectedport-4] <=controlword[0]; //set or no the selected pin
 
 
 end
