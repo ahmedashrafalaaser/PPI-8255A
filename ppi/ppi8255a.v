4bad89dfad47e8b797ebabe9e18078d608a72f0d
omar-basiony <13E0113@eng.asu.edu.eg>
2018-12-06 17:48:45 +0200

    e7na bnthaan

--- ppi/ppi8255a.v
+++ ppi/ppi8255a.v
@@ -27,3 +27,51 @@ portcu o(PortCu,PortD,control,controlword);
 
 
 endmodule 
+module test bench ();
+  PPI8255A test(PortA,PortB,PortD,PortCl,PortCu,nCs,nRe,nWr,Reset,A);
+  wire [7:0] PortA , portB , PortD ;
+  wire [3:0] PortCl , PortCU ;
+  reg nCs, nRe , nWr , reset ;
+  reg [1:0] A,
+  //test case 1
+  initial 
+    begin
+      nCs =0;
+      reset = 0 ;
+      nWr = 0;
+      nRe = 1 ; 
+      A = 2'b11 ;
+      PortD =8'b10000011;
+      #300
+      
+      PortD =8'b10000001;
+      #300
+      
+      PortD =8'b10010000;
+      #300
+      
+      PortD =8'b10001001;
+      #300
+      
+      PortD =8'b10010010;
+      #300
+      
+      PortD =8'b10010011;
+      #300
+      
+      PortD =8'b10011000;
+      #300
+      
+      PortD =8'b10011011;
+      #300
+      
+      PortD =8'b10000011;
+      #300
+      
+      PortD =8'b10000011;
+      #300
+      
+  // test case 2
+  
+  // test case 3 
+  
